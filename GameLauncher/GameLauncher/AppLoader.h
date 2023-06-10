a1f4c0d130e66be1d6d29cb4e6249d3ac35cce41
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2023-06-10 17:41:42 +0900

    ヘッダーの部分を書いた
    AppLoader.hとDefine.hを書きました。
    あと学生作品を試しに表示したいのでゲームも追加した

---
+++ GameLauncher/GameLauncher/AppLoader.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+#include"Define.h"
+
+class Apploader
+{
+	private:
+		//読み込む対象のフォルダ名(今回は./Game)
+		FilePath folderpath;
+
+		//引数のパスが示すフォルダ内の.exeを見つけてそのパスを返す
+		FilePath SearchExePath(const FilePath& path);
+
+		//引数のパスが示すフォルダ内のtitlesceneを見つけてTextureとして読み込みを返す
+		Texture SearchTitleScene(const FilePath& path);
+
+		//引数のパスが示すフォルダ内のgamemainを見つけてTextureとして読み込みを返す
+		Texture SearchGameMain(const FilePath& path);
+
+		//引数のパスが示すフォルダ内のreadme.txtを見つけて中身を全部読み込みを返す
+		String SearchReadMe(const FilePath& path);
+
+	public:
+		//読み込む対象のフォルダのパスを引数としたコンストラクタ
+		Apploader(const FilePath& path);
+
+		//ゲームやスクショなどのデータを全部読み込んでApplication(自作の構造体)の可変長配列として返す
+		Array<Application> Load();
+};
---
+++ GameLauncher/GameLauncher/Define.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+//.exeのパスやスクショなど読み込んだものをまとめて保存
+struct Application
+{
+	unsigned int id;
+	String name;
+	FilePath exepath;
+	Texture titlescene;
+	Texture gamemain;
+	String readme;
+};

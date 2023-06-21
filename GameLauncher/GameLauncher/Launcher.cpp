fdedf020f76ca44d96d3814e72ebf8e7db3b1b8d
okuda-ryunosuke27 <okuda-r@kbcgroup.ac.jp>
2023-06-21 11:12:22 +0900

    前できなかったところができたかも
    processinfoの部分が昔のものだったかもしれないので
    siv3dのクラス一覧を見たらそれっぽいものを見つけたためそれを代用する

---
+++ GameLauncher/GameLauncher/Launcher.cpp
@@ -0,0 +1,30 @@
+﻿#include"Launcher.h"
+
+Launcher::Launcher(const FilePath& path) :apploader(path)
+{
+	applications = apploader.Load();
+
+
+	//描画などに必要な変数や関数
+}
+
+void Launcher::Update()
+{
+	//ゲームが起動中だとtrue、そうでない場合falseとなる
+	if (procsse)
+	{
+		if (procsse->isRunning())
+		{
+			Window::Minimize();//ゲーム起動中はランチャーを最小化
+		}
+		else
+		{
+			Window::Restore();//ゲームが終了したらランチャーの最小化を解除
+			procsse = none;//ゲームが起動していない状態をセット
+		}
+	}
+	else
+	{
+
+	}
+}
---
+++ GameLauncher/GameLauncher/Launcher.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include"AppLoader.h"
+#include <Siv3D.hpp>
+class Launcher
+{
+	private:
+		Array<Application> applications;
+		AppLoader apploader;
+		Optional<ChildProcess> procsse;
+
+	public:
+		Launcher(const FilePath& path);
+
+		//入力などの処理
+		void Update();
+
+		//描画処理
+		void Draw()const;
+
+};
